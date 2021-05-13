eaf2a6db0e48bf05a662d43d3c34455a50cfcea9
trebsirk <trebsirk@gmail.com>
2021-05-13 04:41:39 -0400

    added new file

---
+++ dsinit-pipfile.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# Script to automate the environment creation for data science python projects. 
+# 2021-05-13
+
+# if lines below don't work, try this: sudo pip install pipenv
+pipenv install
+pipenv check
